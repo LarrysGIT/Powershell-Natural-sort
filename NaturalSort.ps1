54e1b143f959a466f1888098a1541553c09746ce
Larry Song <Larry.Song@civica.com.au>
2018-01-19 12:09:29 +1100

    first commit

---
+++ NaturalSort.ps1
@@ -0,0 +1,38 @@
+﻿
+function Sort-Naturally
+{
+    PARAM(
+        [string[]]$strArray
+    )
+
+    Add-Type -TypeDefinition @'
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace NaturalSort {
+    public static class NaturalSort
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        public static extern int StrCmpLogicalW(string psz1, string psz2);
+
+        public static System.Collections.ArrayList Sort(System.Collections.ArrayList foo)
+        {
+            foo.Sort(new NaturalStringComparer());
+            return foo;
+        }
+    }
+
+    public class NaturalStringComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return NaturalSort.StrCmpLogicalW(x.ToString(), y.ToString());
+        }
+    }
+}
+'@
+
+    return [NaturalSort.NaturalSort]::Sort($strArray)
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Sort?
+## You may notice "sort" in Windows explorer and other programs are different. Why?
+## Windows explorer is using `natural sort` which in many cases like "humans"
+
+# Natural sort?
+## Windows explorer is using a legacy API in `shlwapi.dll` called `StrCmpLogicalW`
+
+# I want use `Naturall sort` in my powershell script.
+## Not that easy and not tidy, but, achievable.
+
+'''
+PS> # Natural sort
+PS> . .\NaturalSort.ps1
+PS> Sort-Naturally -strArray @('2', '1', '11')
+1
+2
+11
+PS> # If regular sort is used
+PS> @('2', '1', '11') | Sort-Object
+1
+11
+2
+'''
+
+## The script is simple enough while probably not tidy.
+
+- Larry.Song@outlook.com
